958365b93a4575cae9335437dfa9839e74d3e70d
me33x3 <me33x3@gmail.com>
2021-11-17 00:12:09 +0900

    algorithm-study

--- baekjoon/7576. 토마토/submit.py
+++ baekjoon/7576. 토마토/submit.py
@@ -6,26 +6,29 @@ store, queue = [], deque()
 for n in range(N):
     store.append(list(map(int, input().split())))
 
-    for m in store[-1]:
-        if m == 1:
+    for m in range(M):
+        if store[n][m] == 1:
             queue.append([n, m])
 
 answer = 0
 dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
-while queue:
-    n, m = queue.popleft()
+while True:
     tmp = deque()
+    while queue:
+        n, m = queue.popleft()
 
-    for x, y in zip(dx, dy):
-        mx, my = n + x, m + y
+        for x, y in zip(dx, dy):
+            mx, my = m + x, n + y
 
-        if mx > -1 and mx < N and my > -1 and my < M:
-            if store[mx][my] == 0:
-                store[mx][my] = 1
-                tmp.append([mx, my])
+            if mx > -1 and mx < M and my > -1 and my < N:
+                if store[my][mx] == 0:
+                    store[my][mx] = 1
+                    tmp.append([my, mx])
 
-    if tmp:
-        answer += 1
-        queue = tmp
+    if not tmp:
+        break
+
+    answer += 1
+    queue = tmp
 
 print(answer if all(0 not in l for l in store) else -1)
