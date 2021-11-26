21ed1bd1bde1a32ce80e65c786a62379f8423a51
me33x3 <me33x3@gmail.com>
2021-11-27 01:07:11 +0900

    algorithm-study

---
+++ baekjoon/9184. 신나는 함수 실행/submit.py
@@ -0,0 +1,22 @@
+MAX = 21
+mem = [[[0] * MAX for _ in range(MAX)] for _ in range(MAX)]
+
+def w(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
+    if a > 20 or b > 20 or c > 20:
+        return w(20, 20, 20)
+
+    if not mem[a][b][c]:
+        if a < b < c:
+            mem[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
+        else:
+            mem[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1)
+
+    return mem[a][b][c]
+
+while True:
+    a, b, c = map(int, input().split())
+    if a == -1 and b == -1 and c == -1:
+        break
+    print('w({}, {}, {}) = {}'.format(a, b, c, w(a, b, c)))
