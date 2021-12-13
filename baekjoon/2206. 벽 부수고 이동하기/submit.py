9839f8c3ea3c30dbba42aa760af1ed20f7e3e1af
me33x3 <me33x3@gmail.com>
2021-12-13 17:18:16 +0900

    algorithm-study

--- baekjoon/2206. 벽 부수고 이동하기/submit.py
+++ baekjoon/2206. 벽 부수고 이동하기/submit.py
@@ -1,28 +1,31 @@
 from collections import deque
-import math, sys
+import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
+def bfs():
+    dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
+    queue = deque([[0, 0, False]])
+    move[0][0] = 1
 
+    while queue:
+        n, m, b = queue.popleft()
+
+        if n == N - 1 and m == M - 1:
+            return move[-1][-1]
+
+        for x, y in zip(dx, dy):
+            mx, my = m + x, n + y
+
+            if 0 <= mx < M and 0 <= my < N:
+                if (not board[my][mx] and b) or (board[my][mx] and not b):
+                    if move[my][mx] >= move[n][m] + 1:
+                        move[my][mx] = move[n][m] + 1
+                        queue.append([my, mx, True if board[my][mx] else b])
+    return -1
+
+N, M = map(int, input().split())
 board, move = [], []
 for _ in range(N):
-    board.append(list(map(int,input().rstrip())))
+    board.append(list(map(int, input().rstrip())))
     move.append([float('inf') for _ in range(M)])
-
-dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
-queue = deque([[0, 0, False]])
-move[0][0] = 1
-
-while queue:
-    n, m, b = queue.popleft()
-
-    for x, y in zip(dx, dy):
-        mx, my = m + x, n + y
-
-        if 0 <= mx < M and 0 <= my < N:
-            if not board[my][mx] or (board[my][mx] and not b):
-                if move[my][mx] >= move[n][m] + 1:
-                    move[my][mx] = move[n][m] + 1
-                    queue.append([my, mx, True if board[my][mx] else b])
-
-print(-1 if math.isinf(move[-1][-1]) else move[-1][-1])
+print(bfs())
---
+++ programmers/더 맵게/solution.py
@@ -0,0 +1,4 @@
+def solution(scoville, K):
+    return 0
+
+print(solution([1, 2, 3, 9, 10, 12], 7)) # 2
