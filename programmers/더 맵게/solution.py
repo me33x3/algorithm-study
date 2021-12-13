8c8435be8558dbfa9cf91767f63ee88d90f13cc3
me33x3 <me33x3@gmail.com>
2021-12-13 21:34:25 +0900

    algorithm-study

--- programmers/더 맵게/solution.py
+++ programmers/더 맵게/solution.py
@@ -1,4 +1,20 @@
+import heapq
+
 def solution(scoville, K):
-    return 0
+    answer = 0
+
+    heapq.heapify(scoville)
+
+    while scoville[0] < K:
+        i = heapq.heappop(scoville)
+        try:
+            j = heapq.heappop(scoville)
+        except:
+            answer = -1
+            break
+        heapq.heappush(scoville, i + j * 2)
+        answer += 1
+
+    return answer
 
 print(solution([1, 2, 3, 9, 10, 12], 7)) # 2
