66cfaa3efb882657aaf6939f579233b914619876
me33x3 <me33x3@gmail.com>
2021-12-17 10:09:46 +0900

    algorithm-study

--- baekjoon/1920. 수 찾기/submit.py
+++ baekjoon/1920. 수 찾기/submit.py
@@ -1,10 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-input()
+def search(num):
+    left, right = 0, N - 1
+    mid = (left + right) // 2
+
+    while left <= right:
+        if A[mid] < num:
+            left = mid + 1
+        elif A[mid] > num:
+            right = mid - 1
+        else:
+            return 1
+
+        mid = (left + right) // 2
+
+    return 0
+
+N = int(input())
 A = list(map(int, input().split()))
 input()
-B = list(map(int, input().split()))
 
-for i in B:
-    print(1 if i in A else 0)
+A.sort()
+for i in list(map(int, input().split())):
+    print(search(i))
