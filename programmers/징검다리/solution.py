fa9da5688e955e86d91ef742d9c0bf01712388b4
me33x3 <me33x3@gmail.com>
2021-12-27 22:09:11 +0900

    algorithm-study

--- programmers/징검다리/solution.py
+++ programmers/징검다리/solution.py
@@ -1,7 +1,23 @@
 def solution(distance, rocks, n):
+    rocks.extend([0, distance])
     rocks.sort()
-    left, right = 1, distance
 
-    return 0
+    left, right = 0, distance
+    while left <= right:
+        mid = (left + right) // 2
+        curr, cnt = 0, 0
+
+        for i in range(1, len(rocks)):
+            if rocks[i] - curr < mid:
+                cnt += 1
+            else:
+                curr = rocks[i]
+
+        if n >= cnt:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return right
 
 print(solution(25, [2, 14, 11, 21, 17], 2)) # 4
