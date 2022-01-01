e8b02a1143cb44a020771bc3015e267bd9d4a86a
me33x3 <me33x3@gmail.com>
2022-01-01 22:57:19 +0900

    algorithm-study

--- baekjoon/13305. 주유소/submit.py
+++ baekjoon/13305. 주유소/submit.py
@@ -2,5 +2,11 @@ N = int(input())
 dist = list(map(int, input().split()))
 price = list(map(int, input().split()))
 
+min = float('inf')
+answer = 0
 for i in range(N - 1):
-    pass
+    if min > price[i]:
+        min = price[i]
+    answer += min * dist[i]
+
+print(answer)
