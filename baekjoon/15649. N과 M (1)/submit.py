f85b06176912a3420a934329a6ad9ebcdc675b40
me33x3 <me33x3@gmail.com>
2022-01-20 19:24:40 +0900

    algorithm-study

---
+++ baekjoon/15649. N과 M (1)/submit.py
@@ -0,0 +1,17 @@
+def dfs(s, v):
+    if len(s) == M:
+        for num in s:
+            print(num, end=' ')
+        print()
+
+    for i in range(1, N + 1):
+        if i not in v:
+            s.append(i)
+            v.append(i)
+            dfs(s, v)
+            v.pop()
+            s.pop()
+
+N, M = map(int, input().split())
+for i in range(1, N + 1):
+    dfs([i], [i])
