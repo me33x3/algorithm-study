95b20d8a853b0dc1162d9d1942ca4fee7c7c41da
me33x3 <me33x3@gmail.com>
2022-02-15 15:15:04 +0900

    algorithm-study

---
+++ baekjoon/10866/submit.py
@@ -0,0 +1,25 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+deq = deque()
+for t in range(int(input())):
+    cmd = input().split()
+
+    if cmd[0] == 'push_front':
+        deq.appendleft(cmd[1])
+    elif cmd[0] == 'push_back':
+        deq.append(cmd[1])
+    elif cmd[0] == 'pop_front':
+        print(deq.popleft() if deq else -1)
+    elif cmd[0] == 'pop_back':
+        print(deq.pop() if deq else -1)
+    elif cmd[0] == 'size':
+        print(len(deq))
+    elif cmd[0] == 'empty':
+        print(0 if deq else 1)
+    elif cmd[0] == 'front':
+        print(deq[0] if deq else -1)
+    elif cmd[0] == 'back':
+        print(deq[-1] if deq else -1)
