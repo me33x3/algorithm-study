eae52075635a86fca79bac8ee874bc2f42035944
me33x3 <me33x3@gmail.com>
2022-03-07 11:01:25 +0900

    algorithm-study

---
+++ baekjoon/11376/submit.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(x):
+    for i in a[x]:
+        if visited[i]:
+            continue
+        visited[i] = True
+
+        if w[i] == 0 or dfs(w[i]):
+            w[i] = x
+            return True
+    return False
+
+n, m = map(int, input().split())
+a = {}
+w = [0] * (m + 1)
+
+for i in range(1, n + 1):
+    a[i] = list(map(int, input().split()))[1:]
+
+answer = 0
+for _ in range(2):
+    for i in range(1, n + 1):
+        visited = [False] * (m + 1)
+        answer += 1 if dfs(i) else 0
+print(answer)
