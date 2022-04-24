ed9265ff9a13e65ee124b6d65af1239692225aa3
me33x3 <me33x3@gmail.com>
2022-04-24 16:33:58 +0900

    1927

---
+++ baekjoon/1927/1927.py
@@ -0,0 +1,10 @@
+import sys, heapq
+input = sys.stdin.readline
+
+heap = []
+for _ in range(int(input())):
+    n = int(input())
+    if n == 0:
+        print(heapq.heappop(heap) if heap else 0)
+    else:
+        heapq.heappush(heap, n)
