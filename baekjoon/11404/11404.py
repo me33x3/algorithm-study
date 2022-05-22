85a6e1ef2a9cbea5d93d32365068bf7662356e5b
me33x3 <me33x3@gmail.com>
2022-05-22 18:12:12 +0900

    11404

---
+++ baekjoon/11404/11404.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+INF = sys.maxsize
+
+n = int(input())
+m = int(input())
+
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    graph[i][i] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = min(graph[a][b], c)
+
+for i in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][i] + graph[i][b])
+
+for line in graph[1:]:
+    for c in line[1:]:
+        print(c if c != INF else 0, end=' ')
+    print()
