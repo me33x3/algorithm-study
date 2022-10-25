2f4d35c31b7b6ec93dad130e6e92dcd9ca25f14e
me33x3 <me33x3@gmail.com>
2022-10-25 16:57:50 +0900

    9084

---
+++ baekjoon/9084/9084.py
@@ -0,0 +1,14 @@
+for _ in range(int(input())):
+    n = int(input())
+    coins = list(map(int, input().split()))
+    m = int(input())
+
+    dp = [0] * (m + 1)
+    dp[0] = 1
+
+    for coin in coins:
+        for i in range(1, m + 1):
+            if i >= coin:
+                dp[i] += dp[i-coin]
+
+    print(dp[-1])
